c601bb59741979505adbd3ae3228b7b818911ff7
pjh <junghun5947@gmail.com>
2021-01-12 16:26:11 +0900

    Initial commit

---
+++ src/main/java/com/pjh/springgraphqlapi/SpringGraphqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.pjh.springgraphqlapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGraphqlApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pjh/springgraphqlapi/SpringGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pjh.springgraphqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
